29440aa7c4f1b8e13a2d67c3df7a0d803bb00a72
perkasalintang52 <98840516+perkasalintang52@users.noreply.github.com>
2022-06-28 00:02:58 +0700

    Update config.js

--- config.js
+++ config.js
@@ -22,16 +22,16 @@ global.APIKeys = {
 }
 
 //═══════[modification]════════\\
-global.owner = ['6288217843188','6288217843188'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
-global.pemilik = ['6288217843188'] //another owner number
-global.premium = ['6288217843188'] //premium number
-global.pengguna = 'Reza Temayang' //username
-global.botnma = 'Bot rex' //bot name
-global.ownernma = 'Reza Temayang' //owner name
-global.packname = 'Bot rex' //sticker package name
-global.author = 'By Reza' //sticker author name
+global.owner = ['6281322577123','6281322577123'] //owner number, u can put multiple owner number, within quotations and seperated by comma.
+global.pemilik = ['6281322577123'] //another owner number
+global.premium = ['6281322577123'] //premium number
+global.pengguna = 'LINTANG' //username
+global.botnma = 'BOT LINTANG' //bot name
+global.ownernma = 'LINTANG' //owner name
+global.packname = 'BOT LINTANG' //sticker package name
+global.author = 'By LINTANG' //sticker author name
 global.sessionName = 'session' //session name
-global.prefa = ['#','!','/',''] //prefix
+global.prefa = [''] //prefix
 global.sp = '⭔' //design
 global.mess = {
     success: 'Done ✓',
